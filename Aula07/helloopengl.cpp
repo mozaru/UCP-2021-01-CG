f723fd0f08827e43f50593735db6a71d0092086b
mozaru <mozar.silva@gmail.com>
2021-04-05 20:21:01 -0300

    aula 07 - texto, textura, teclado e mouse

--- Aula07/helloopengl.cpp
+++ Aula07/helloopengl.cpp
@@ -5,6 +5,7 @@
 #include<QDateTime>
 #include<QMouseEvent>
 #include<QKeyEvent>
+#include<QPainter>
 
 typedef struct
 {
@@ -17,10 +18,54 @@ HelloOpenGL::HelloOpenGL(QWidget *parent) : QOpenGLWidget(parent)
 
     this->setFocusPolicy(Qt::StrongFocus);
     this->setFocus();
+
+
+    connect(this, &HelloOpenGL::invalidate, this, [this] {
+        qDebug() << "invalidate";
+         //m_context->makeCurrent(m_surf);
+        this->makeCurrent();
+        this->paintGL();
+        this->doneCurrent();
+        // m_context->doneCurrent();
+        this->glFlush();
+        //m_context->swapBuffers(m_surf);
+    }, Qt::QueuedConnection);
 }
 
+
+
+GLuint LoadGLTextures(QString path){
+
+    QImage img;
+    GLuint tex;
+
+    if(!img.load(path)){
+
+        qDebug()<<"Image loading failed";
+    }
+
+    QImage t = (img.convertToFormat(QImage::Format_RGBA8888)).mirrored();
+
+    glGenTextures(1, &tex);
+
+    glBindTexture(GL_TEXTURE_2D, tex);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, 3, t.width(), t.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, t.bits());
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glBindTexture( GL_TEXTURE_2D, 0);
+
+
+    return tex;
+}
+
+GLuint textura;
 void HelloOpenGL::initializeGL()
 {
+    m_context = context()->currentContext();
+    m_surf = context()->currentContext()->surface();
     //QOpenGLFunctions *glFuncs = QOpenGLContext::currentContext()->functions();
     //glFuncs->glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
     this->initializeOpenGLFunctions();
@@ -34,6 +79,10 @@ void HelloOpenGL::initializeGL()
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    textura = LoadGLTextures("C:\\Aulas\\202101- UCP - CG\\Aula07\\azulejo.jpg");
+
+    glEnable(GL_TEXTURE_2D);
+
 }
 
 void HelloOpenGL::resizeGL(int width, int height)
@@ -42,27 +91,14 @@ void HelloOpenGL::resizeGL(int width, int height)
     Q_UNUSED(height);
 }
 
-void HelloOpenGL::renderText(double x, double y, QString text)
+void HelloOpenGL::renderText(int x, int y, QString text)
 {
-/*    int width = this->width();
-    int height = this->height();
-
-    GLdouble model[4][4], proj[4][4];
-    GLint view[4];
-    glGetDoublev(GL_MODELVIEW_MATRIX, &model[0][0]);
-    glGetDoublev(GL_PROJECTION_MATRIX, &proj[0][0]);
-    glGetIntegerv(GL_VIEWPORT, &view[0]);
-    GLdouble textPosX = 0, textPosY = 0, textPosZ = 0;
-
-    textPosY = height - textPosY; // y is inverted
-
-    QPainter *painter = (QPainter *)this->sharedPainter();
-    painter->setPen(Qt::yellow);
-    painter->setFont(QFont("Helvetica", 8));
+    QPainter painter(this);
+    painter.setPen(Qt::yellow);
+    painter.setFont(QFont("Helvetica", 16));
     //painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
-    painter->drawText(textPosX, textPosY, text); // z = pointT4.z + distOverOp / 4
-    painter->end();
-*/
+    painter.drawText(x, y, text); // z = pointT4.z + distOverOp / 4
+    painter.end();
 }
 
 void HelloOpenGL::keyPressEvent(QKeyEvent* e)
@@ -97,9 +133,15 @@ void HelloOpenGL::mouseReleaseEvent(QMouseEvent *q)
 }
 
 
+/*void HelloOpenGL::invalidate()
+{
+
+}*/
+
 void HelloOpenGL::paintGL()
 {
     static int contFrames=0;
+    static int FPS = 0;
     static long long lastTime=QDateTime::currentMSecsSinceEpoch();
 
     static double ang=0;
@@ -107,20 +149,44 @@ void HelloOpenGL::paintGL()
     glViewport((this->width()-max)/2, (this->height()-max)/2, max, max);
     //glViewport(0,0, this->width(), this->height());
 
+    glBindTexture( GL_TEXTURE_2D, 0);
+
+
+    float final = 1.0;
+    glPushMatrix();
+    glColor3d(0,0,0);
+    glBegin(GL_QUADS);
+            //glTexCoord2f(0,0);
+            glVertex3f(-1,-1,0);
+            //glTexCoord2f(final,0);
+            glVertex3f( 1,-1,0);
+            //glTexCoord2f(final,final);
+            glVertex3f( 1, 1,0);
+            //glTexCoord2f(0,final);
+            glVertex3f(-1, 1,0);
+        glEnd();
+    //
+    //glDisable(GL_TEXTURE_2D);
+    glPopMatrix();
+
 
     glPushMatrix();
     ang += 1;
     glTranslatef(cx,cy,0);
     glRotatef(ang,0,0,1);
     //aqui fazemos nosso desenho
-    glColor3d(255,0,0);
+    glColor3d(0,255,255);
+    glBindTexture( GL_TEXTURE_2D, textura);
     glBegin(GL_TRIANGLE_STRIP);
-        glVertex2f(0,-0.5);
+        glTexCoord2f(1.5,0);
+        glVertex2f(0.0,-0.5);
+        glTexCoord2f(3,3);
         glVertex2f(0.5,0.5);
+        glTexCoord2f(0,3);
         glVertex2f(-0.5,0.5);
     glEnd();
     glPopMatrix();
-
+    glBindTexture( GL_TEXTURE_2D, 0);
 
     glPushMatrix();
     glTranslatef(cx,cy,0);
@@ -131,15 +197,16 @@ void HelloOpenGL::paintGL()
     glEnd();
     glPopMatrix();
 
-
     contFrames++;
     if (QDateTime::currentMSecsSinceEpoch()-lastTime > 1000)
     {
-        std::cout << "FPS:" << contFrames << std::endl;
+        //std::cout << "FPS:" << contFrames << std::endl;
+        FPS = contFrames;
         contFrames = 0;
         lastTime = QDateTime::currentMSecsSinceEpoch();
     }
-
+    QString s= QString("FPS:%1").arg(FPS);
+    renderText(10,25,s);
 
     this->update();
 }
--- Aula07/helloopengl.h
+++ Aula07/helloopengl.h
@@ -14,14 +14,16 @@ protected:
     void initializeGL() override;
     void resizeGL(int width, int height) override;
     void paintGL() override;
-    void renderText(double x, double y, QString text);
+    void renderText(int x, int y, QString text);
     void mouseReleaseEvent(QMouseEvent *q) override;
     void keyPressEvent(QKeyEvent* e) override;
 private:
     double cx,cy;
+    QOpenGLContext *m_context;
+    QSurface *m_surf;
 
 signals:
-
+    void invalidate();
 };
 
 #endif // HELLOOPENGL_H
